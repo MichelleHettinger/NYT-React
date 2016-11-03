48fe15e5bfd534677d6c31c710703b19ebe5347a
Michelle Hettinger <Michelle Hettinger>
2016-11-02 20:19:16 -0400

    edited the html to make it compatible with reacts compiler. all components of website now render after compiling with webpack

--- app/Components/Main.js
+++ app/Components/Main.js
@@ -2,8 +2,8 @@
 var React = require('react');
 
 // Here we include all of the sub-components
-var Saved = require('./Saved');
-var Search = require('./Search');
+var Saved = require('./Saved.js');
+var Search = require('./Search.js');
 
 // This is the main component. It includes the banner and button.
 // Whenever you click the button it will communicate the click event to all other sub components.
@@ -11,49 +11,40 @@ var Main = React.createClass({
 
 	// Here we render the function
 	render: function(){
-
 		return(
 
 			<div className="container">
 
-				// Jumbotron for Title
-				<div className="jumbotron" style="background-color: #20315A ; color: white;">
+				<div className="jumbotron">
 					<h1 className="text-center"><strong><i className="fa fa-newspaper-o"></i> New York Times Search</strong></h1>
 				</div>
 
-				// Row for Searching New York Times
 				<div className="row">
 					<div className="col-sm-12">
-					<br>
-						// First panel is for handling the search parameters -->
+					<br/>
 						<div className="panel panel-primary">
 							<div className="panel-heading">
 								<h3 className="panel-title"><strong><i className="fa  fa-list-alt"></i>   Search Parameters</strong></h3>
 							</div>
 							<div className="panel-body">
 
-								// Here we create an HTML Form for handling the inputs-->
 								<form role="form">
 
-							  	  // Here we create the text box for capturing the search term-->
 								  <div className="form-group">
 								    <label for="search">Search Term:</label>
-								    <input type="text" className="form-control" id="searchTerm">
+								    <input type="text" className="form-control" id="searchTerm" />
 								  </div>
 
-							  	  // Here we capture the Start Year Parameter-->
 								  <div className="form-group">
 								    <label for="startYear">Start Year (Optional):</label>
-								    <input type="text" className="form-control" id="startYear">
+								    <input type="text" className="form-control" id="startYear" />
 								  </div>
 
-							  	  // Here we capture the End Year Parameter -->
 								  <div className="form-group">
 								    <label for="endYear">End Year (Optional):</label>
-								    <input type="text" className="form-control" id="endYear">
+								    <input type="text" className="form-control" id="endYear" />
 								  </div>
 
-								  // Here we have our final submit button -->
 								  <button type="submit" className="btn btn-default" id="runSearch"><i className="fa fa-search"></i> Search</button>
 			  					  <button type="button" className="btn btn-default" id="clearAll"><i className="fa fa-trash"></i> Clear Results</button>
 
@@ -63,20 +54,32 @@ var Main = React.createClass({
 					</div>
 				</div>
 
-				// This row will handle all of the retrieved articles -->
-
-
-				// This row will handle all of the saved articles -->
 
 
 
-				// Footer Region -->
+				<div className="row">
+					<div className="col-sm-12">
+						<br/>
+						<Search/>
+					</div>
+				</div>						
+
+
+				<div className="row">
+					<div className="col-sm-12">
+						<br/>
+						<Saved/>
+					</div>
+				</div>
+
+
+
+
 				<div className="row">
 					<div className="col-sm-12">
 						
-						// Line Break followed by closing -->
-						<hr>
-						<h5 className="text-center"><small>Made by Ahmed with lots and lots of <i className="fa fa-heart"></i></small></h5>
+						<hr/>
+						<h5 className="text-center"><small>Made by Michelle with lots and lots of <i className="fa fa-heart"></i></small></h5>
 
 					</div>
 				</div>
--- app/Components/Saved.js
+++ app/Components/Saved.js
@@ -3,36 +3,28 @@ var React = require('react');
 
 // This is the main component. It includes the banner and button.
 // Whenever you click the button it will communicate the click event to all other sub components.
-var Main = React.createClass({
+var Saved = React.createClass({
 
 	// Here we render the function
 	render: function(){
 
 		return(
 
-				// This row will handle all of the saved articles -->
-				<div className="row">
-					<div className="col-sm-12">
-					<br>
+			// This panel will initially be made up of a panel and wells for each of the articles saved -->
+			<div className="panel panel-primary">
 
-						// This panel will initially be made up of a panel and wells for each of the articles saved -->
-						<div className="panel panel-primary">
-
-							// Panel Heading for the saved articles box -->
-							<div className="panel-heading">
-								<h3 className="panel-title"><strong><i className="fa fa-table"></i>   Saved Articles</strong></h3>
-							</div>
-
-							// This main panel will hold each of the saved articles -->
-							<div className="panel-body" id="wellSection">
-							</div>
-						</div>
-					</div>
+				<div className="panel-heading">
+					<h3 className="panel-title"><strong><i className="fa fa-table"></i>   Saved Articles</strong></h3>
 				</div>
+
+				<div className="panel-body" id="wellSection">
+				</div>
+			</div>
+
 
 		)
 	}
 });
 
-// Export the componen back for use in other files
+// Export the component back for use in other files
 module.exports = Saved;
